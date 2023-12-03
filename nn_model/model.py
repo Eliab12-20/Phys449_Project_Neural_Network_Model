4bf0a820e14eca5468fb6e0f20d404b2048ee7ee
Pranav Sairam <yasairam@uwaterloo.ca>
2023-12-03 18:53:17 -0500

    Finetuning Hyperparameters

--- nn_model/model.py
+++ nn_model/model.py
@@ -12,10 +12,10 @@ class ExoplanetModel(nn.Module):
         self.fc6 = nn.Linear(hidden_size5, output_size)
         
     def forward(self, x):
-        x = F.relu(self.fc1(x))  # Apply ReLU activation to the first layer
-        x = F.relu(self.fc2(x))  # Apply ReLU activation to the second layer
-        x = F.relu(self.fc3(x))  # Apply ReLU activation to the third layer
-        x = F.relu(self.fc4(x))  # Apply ReLU activation to the fourth layer
-        x = F.relu(self.fc5(x))  # Apply ReLU activation to the fifth layer
-        x = self.fc6(x)          # No activation function on the output layer
+        x = F.leaky_relu(self.fc1(x))  # Apply Leaky ReLU activation to the first layer
+        x = F.leaky_relu(self.fc2(x))  # Apply Leaky ReLU activation to the second layer
+        x = F.leaky_relu(self.fc3(x))  # Apply Leaky ReLU activation to the third layer
+        x = F.leaky_relu(self.fc4(x))  # Apply Leaky ReLU activation to the fourth layer
+        x = F.leaky_relu(self.fc5(x))  # Apply Leaky ReLU activation to the fifth layer
+        x = self.fc6(x)                # No activation function on the output layer
         return x
